9a7a8dd91814ab3a362fd3ec0b24a0475bf04565
Aroa Cotrufo <aroa.c97@gmail.com>
2026-02-06 10:00:10 +0100

    correzione es. mail

--- mail/script.js
+++ mail/script.js
@@ -3,12 +3,15 @@ const mail = ['lucagmail.com' , 'andreagmail.com' , 'fabiogmail.com' , 'diegogma
 
 //creare un prompt per far scrivere all'utente la propria mail
 let mailUtente = prompt('Inserisci la tua mail')
-let message = 'Non sei registrato'
+let found = false
 //controllare se la mailUtente è all'interno dell'array mail
 for(let i=0; i<mail.length; i++){
     if(mail[i]===mailUtente){
-        message = 'Accedi'
-        break
+        found = true  
     }   
 }
-console.log(message)
+if(found){
+    console.log('Accedi')
+}else{
+    console.log('Non sei registrato')
+}
